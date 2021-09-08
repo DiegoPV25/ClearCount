937b69d8ebaf186ca6f33d7544fb007f50ef8b01
Diego Padilla <diegotails@hotmail.com>
2021-09-07 19:01:36 -0500

    Agregar archivos de proyecto.

---
+++ PopulationHostedService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClearCount
+{
+    public class PopulationHostedService: IHostedService, IDisposable
+    {
+
+        private Timer _timer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(SendInfo, null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+            return Task.CompletedTask;
+        }
+
+        private void SendInfo(object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
